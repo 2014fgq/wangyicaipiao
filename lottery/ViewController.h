bf5990b3c6c669d298f7ea4e35c0f3d4ffaf7d96
IMAC <imac@imacdeimac.local>
2016-03-27 22:07:26 -0700

    Initial Commit

---
+++ lottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lottery
+//
+//  Created by IMAC on 16/3/27.
+//  Copyright © 2016年 FZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
